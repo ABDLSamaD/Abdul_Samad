74a3bb00ad556b1989accc8f39fe9e58ae046088
ABDLSamaD <142105107+ABDLSamaD@users.noreply.github.com>
2024-10-15 19:56:52 +0500

    first commit

--- src/Components/MainComponent.jsx
+++ src/Components/MainComponent.jsx
@@ -2,6 +2,7 @@ import Home from "./Pages/Home";
 import About from "./Pages/About";
 import { Routes, Route } from "react-router-dom";
 import Contact from "./Pages/Contact";
+import Pagenotfound from "./ohtercomponent/Pagenotfound";
 
 const MainComponent = () => {
   return (
@@ -9,6 +10,7 @@ const MainComponent = () => {
       <Route path="/Abdul_Samad/" element={<Home />} />
       <Route path="/Abdul_Samad/about" element={<About />} />
       <Route path="/Abdul_Samad/contact" element={<Contact />} />
+      <Route path="/*" element={<Pagenotfound />} />
     </Routes>
   );
 };
